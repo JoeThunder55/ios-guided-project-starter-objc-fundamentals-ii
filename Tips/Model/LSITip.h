832dab88b0f8180e4fbfc665d572544139d2933c
Aaron Haughton <dreamcast2000@outlook.com>
2020-05-21 22:01:36 -0400

    Added Properties.

---
+++ Tips/Model/LSITip.h
@@ -0,0 +1,22 @@
+//
+//  LSITip.h
+//  Tips
+//
+//  Created by Joe on 5/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSITip : NSObject
+
+@property (nonatomic, readonly, copy) NSString *name;
+@property (nonatomic, readonly) double total;
+@property (nonatomic, readonly) NSInteger splitCount;
+@property (nonatomic, readonly) double tipCount;
+
+@end
+
+NS_ASSUME_NONNULL_END
