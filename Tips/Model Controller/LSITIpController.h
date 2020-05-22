5f690ec292bb7a912ac0df642ad65f027f0d86dd
Aaron Haughton <dreamcast2000@outlook.com>
2020-05-21 22:28:59 -0400

    set up public interface for the model controller.

--- Tips/Model Controller/LSITIpController.h
+++ Tips/Model Controller/LSITIpController.h
@@ -16,6 +16,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, readonly, copy) NSArray<LSITip *> *tips;
 
+@property (nonatomic, readonly) NSUInteger tipCount;
+
+- (void)addTip:(LSITip *)aTip;
+
+- (LSITip *)tipAtIndex:(NSUInteger)index;
+
 @end
 
 NS_ASSUME_NONNULL_END
